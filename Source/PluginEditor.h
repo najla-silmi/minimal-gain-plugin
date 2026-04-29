7638249f1222b7a31254746379cf0ae3e66719a3
Najla Silmi <najlasilmi15@gmail.com>
2026-04-29 09:47:05 -0400

    Update PluginEditor.h
    Removed redundant comments

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -26,21 +26,15 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
     
-    // Declare sliderValueChanged function
     void sliderValueChanged (juce::Slider *slider) override;
-    
-    // Declare setupSlider function
+  
     void setupSlider(juce::Slider& slider);
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
     MinimalGainPluginAudioProcessor& audioProcessor;
     
-    // Defining sliders
     juce::Slider inputGainKnob, softClipKnob, outputGainKnob;
     
-    // Defining slider labels
     juce::Label inputGainLabel, softClipLabel, outputGainLabel;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MinimalGainPluginAudioProcessorEditor)
